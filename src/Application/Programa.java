d99a14a35a34564ae62502a0ae96d1b7c86226b6
Murilo Carmo <murilocarmo718@gmail.com>
2025-09-11 20:24:41 -0300

    Classe de entidade departamento

---
+++ src/Application/Programa.java
@@ -0,0 +1,11 @@
+package Application;
+
+import model.entities.Departamento;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Departamento obj = new Departamento("livros", 1);
+        System.out.println(obj);
+    }
+}
---
+++ src/model/entities/Departamento.java
@@ -0,0 +1,47 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departamento implements Serializable {
+    private String nome;
+    private Integer id;
+
+    public Departamento(String nome, Integer id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Departamento that = (Departamento) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento - " + "nome = " + nome + ", id = " + id;
+    }
+}
